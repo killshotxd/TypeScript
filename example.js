9b7452f686e4de64468723448223f408ee1d9209
killshotxd <hassanansari211@gmail.com>
2022-11-06 11:11:03 +0530

    Variables in TS

--- example.js
+++
@@ -1,4 +0,0 @@
-function addShipping(price, shipping) {
-    return price + shipping;
-}
-addShipping(10, 5);
---
+++ variables.ts
@@ -0,0 +1,5 @@
+// string, boolean, null, number, undefined
+
+let myName: string = "";
+myName = "Killshot";
+// myName = 5;
